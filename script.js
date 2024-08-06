1a3f90b298cb8ca96ca7fc31eb03441a777d28ac
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2024-08-06 18:22:35 +0200

    code was formated

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-//prettier
+// prettier-ignore
 function compare(num1, num2) {
-  if (num1 > num2) {
-    return 1;
-  } else if (num1 < num2) {
-    return -1;
-  } else {
-    return 0;
-  }
+  if (num1 > num2) {return 1;}
+  else if (num1 < num2) {return -1;}
+   else {return 0}
 }
 // przykładowy input:
 const res1 = compare(2, 1);
 const res2 = compare(1, 2);
 const res3 = compare(1, 1);
-console.log(res1 + res2 + res3);
+console.log(res1, res2, res3);
