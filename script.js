5232ac9af73deb3ee7cfe538c0aa5add89d879ea
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2024-08-06 17:30:04 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function compare(num1, num2) {
+  if (num1 > num2) {
+    return 1;
+  } else if (num1 < num2) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+// przykładowy input:
+const res1 = compare(2, 1);
+const res2 = compare(1, 2);
+const res3 = compare(1, 1);
+consol.log(res1 + res2 + res3);
